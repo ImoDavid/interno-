741f01177622e35ea6db8d3fe68594375fa62475
Imo David <emperorjagz@gmail.com>
2023-01-18 11:31:09 +0100

    feat: routing implemented

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import { HomePage } from './containers/Homepage';
-import './App.css';
+import { AboutPage } from './containers/Aboutpage';
+import {ServicesPage} from './containers/Servicespage';
+import { ErrorPage } from './containers/Errorpage';
 import {Navbar }from './components/navbar';
 import {Footer} from './components/footer';
+import './App.css';
 
 function App() {
   return (
     <>
     <Navbar/>
-   <HomePage/>
+    <Routes>
+        <Route path='/services' element={<ServicesPage />} />
+        <Route path='/about' element={<AboutPage />} />
+        <Route path='/' element={<HomePage />} />
+        <Route path='*' element={<ErrorPage/>}/>
+      </Routes>
    <Footer/>
     </>
   );
